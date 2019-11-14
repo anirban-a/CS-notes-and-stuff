37b05dc706a2823cc08ca54d58b830e2c720cd01
Anirban Acharya <acharya.anirban9@gmail.com>
2019-11-14 21:21:46 +0530

    Documented Maths/SumOfPairwiseHammingDistance.java

--- Maths/SumOfPairwiseHammingDistance.java
+++ Maths/SumOfPairwiseHammingDistance.java
@@ -1,3 +1,34 @@
+/*
+* Sum of pairwise Hamming Distance.
+* =================================
+* Hamming distance between two non-negative integers is defined as the number of positions at which the corresponding bits are different.
+*
+* For example,
+*
+* HammingDistance(2, 7) = 2, as only the first and the third bit differs in the binary representation of 2 (010) and 7 (111).
+*
+* Given an array of N non-negative integers, find the sum of hamming distances of all pairs of integers in the array.
+* Return the answer modulo 1000000007.
+*/
+
+/*
+
+Approach 1:
+===========
+For every A[i] and A[j] where j>i, find the count of difference in bits and add the count modulo 1000000007 to final answer. Since we counted all distinct
+pairs, we must return ans*2 modulo 1000000007.
+
+Time complexity O(n^2).
+
+
+Approach 2:
+===========
+We will use 2 maps, one to keep a count of set bits for every bit position (LSB through MSB) for all the numbers, and likewise another map to keep count of
+unset bits. Reason for that is for every number we actually need the count of numbers whose i-th bit position differs from the current number which is
+equivalent to total number of bit difference at i-th position with respect to the current number. And for i-th bit position of every number if it is set
+increment set bit count at i-th bit position of the map keeping count of set bits, and same for unset bits.
+
+*/
 import java.io.*;
 import java.util.*;
 public class SumOfPairwiseHammingDistance{
@@ -12,9 +43,11 @@ public class SumOfPairwiseHammingDistance{
             int k=1;
             for(int bit=0;bit<31;bit++){
                 if((x&k)!=0){
+                	// current bit position is set for x so we count the number of unset bits at this position
                     ans=(ans%mod+unsetCount[bit]%mod)%mod;
                     setCount[bit]++;
                 }else{
+                	// current bit position is unset for x so we count the number of set bits at this position
                     ans=(ans%mod+setCount[bit]%mod)%mod;
                     unsetCount[bit]++;
                 }
