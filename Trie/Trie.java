66abbaec7ba56c5e3d838436f4b75d2ee15960a4
Anirban Acharya <acharya.anirban9@gmail.com>
2020-02-01 20:26:34 +0530

    Added word search by prefix, and word list by prefix

--- Trie/Trie.java
+++ Trie/Trie.java
@@ -3,6 +3,7 @@
 * This implementation assumes the words stored are going to be made of only lower case alphabets [a-z].
 **/
 
+import java.util.*;
 class Trie{
     class TrieNode{
         char val;
@@ -12,6 +13,15 @@ class Trie{
         TrieNode(){
             isWord=false;
             child=new TrieNode[26];
+        }
+    }
+
+    private class Pair{
+        TrieNode node;
+        String word;
+        Pair(TrieNode node, String word){
+            this.node=node;
+            this.word=word;
         }
     }
 
@@ -51,8 +61,51 @@ class Trie{
         return currentNode.isWord;
     }
 
+    /** Returns the child node for the given character or null if it doesn't exist. */
     public TrieNode getNode(char chr){
-        // return the child node for the given character or null if it doesn't exist
+
         return this.root.child[chr-'a'];
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix){
+        // if there exists a valid node then the prefix is valid.
+        return getNodeWithPrefix(prefix)!=null;
+    }
+
+    private TrieNode getNodeWithPrefix(String prefix){
+        TrieNode currentNode=root;
+        for(int i=0;i<prefix.length();i++){
+            char c=prefix.charAt(i);
+            int idx=c-'a';
+            if(currentNode.child[idx]==null)
+                return null;
+            currentNode=currentNode.child[idx];
+        }
+        return currentNode;        
+    }
+
+    public List<String> wordListWithPrefix(String prefix){
+        TrieNode node=getNodeWithPrefix(prefix);
+        Stack<Pair> stack=new Stack<>();
+        List<String>wordList = new LinkedList<>();
+        stack.push(new Pair(node,prefix));
+
+        // do DFS to form words.
+
+        while(!stack.empty()){
+            Pair pair=stack.pop();
+            TrieNode currentNode = pair.node;
+            prefix=pair.word;
+            if(currentNode.isWord)
+                wordList.add(pair.word);
+            for(int i=0;i<26;i++){
+                if(currentNode.child[i]!=null){
+                    Pair childPair=new Pair(currentNode.child[i],prefix+currentNode.child[i].val);
+                    stack.push(childPair);
+                }
+            }
+        }
+        return wordList;
     }
 }
